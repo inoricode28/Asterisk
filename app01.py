259ee262e00df76109e53f62ef204142c9ada908
inoricode28 <inori237@gmail.com>
2022-11-03 09:41:16 -0500

    astk03

---
+++ app01.py
@@ -0,0 +1,18 @@
+import socket
+import time
+print("Iniciando Programa")
+s=socket.socket()
+s.connect(("192.168.1.11",5038))
+
+print("Manda Login")
+s.send("Action: Login\n".encode())
+s.send("Username: Inori\n".encode())
+s.send("Secret: 1234\n".encode())
+s.send("\n".encode())
+time.sleep(8)
+
+print("Mandado de Logoff")
+s.send("Action: Logoff\n".encode())
+s.send("\n".encode())
+
+print("Fin de programa")
---
+++ app03.py
@@ -0,0 +1,26 @@
+import socket
+import time
+
+def iniciar_ami(usuario,clave):    
+    s.send("Action: Login\n".encode())
+    comando = "Username: " + usuario + "\n"
+    s.send(comando.encode())
+    comando = "Secret: " + clave + "\n"
+    s.send(comando.encode())
+    s.send("\n".encode())
+
+def finalizar_ami():
+    s.send("Action: Logoff\n".encode())
+    s.send("\n".encode())
+
+print("Iniciando Programa")
+s=socket.socket()
+s.connect(("192.168.1.11",5038))
+
+print("Iniciando Login")
+iniciar_ami("Inori","1234")
+time.sleep(8)
+print("MAndando Logoff")
+finalizar_ami()
+
+print("Fin de programa")
