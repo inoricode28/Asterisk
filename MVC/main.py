c2d8aa657f8d09c310a63030499d988195662601
inoricode28 <inori237@gmail.com>
2022-11-10 17:38:31 -0500

    astk07

---
+++ MVC/main.py
@@ -0,0 +1,23 @@
+########################################################################
+# IMPORT CUSTOM BUTTONS FILE
+# Because ill be frequently updating this package,
+# run "pip install --upgrade QT-PyQt-PySide-Custom-Widgets"
+# to install any updates
+from Custom_Widgets.Widgets import *
+
+from PySide2.QtCore import *
+########################################################################
+#QCustomQPushButton esto se pone en la particula
+# aqui se ejecuta todo el programa
+import sys
+# ------ Importacion PySide2 ------
+#from PySide2 import QtWidgets
+#from PySide2 import QtCore
+from controlador.botones import Clark #tambien se modificable
+#Una modificacion
+if __name__=='__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    ventana_ses = Clark()#Clark es modificable
+    #ventana_ses = Tranferencia()
+    ventana_ses.show()
+    sys.exit(app.exec_())
---
+++ MVC/vista/ui_botones.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'botonestbPQtR.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+from Custom_Widgets.Widgets import *
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(816, 394)
+        MainWindow.setStyleSheet(u"background-color: rgb(24, 24, 36);")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.pushButton = QCustomQPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(10, 50, 251, 91))
+        self.pushButton_2 = QCustomQPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_2.setGeometry(QRect(270, 50, 251, 91))
+        self.pushButton_3 = QCustomQPushButton(self.centralwidget)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+        self.pushButton_3.setGeometry(QRect(530, 50, 251, 91))
+        self.pushButton_4 = QCustomQPushButton(self.centralwidget)
+        self.pushButton_4.setObjectName(u"pushButton_4")
+        self.pushButton_4.setGeometry(QRect(10, 150, 251, 91))
+        self.pushButton_5 = QCustomQPushButton(self.centralwidget)
+        self.pushButton_5.setObjectName(u"pushButton_5")
+        self.pushButton_5.setGeometry(QRect(270, 150, 251, 91))
+        self.pushButton_6 = QCustomQPushButton(self.centralwidget)
+        self.pushButton_6.setObjectName(u"pushButton_6")
+        self.pushButton_6.setGeometry(QRect(530, 150, 251, 91))
+        font = QFont()
+        font.setPointSize(36)
+        self.pushButton_6.setFont(font)
+        self.pushButton_7 = QCustomQPushButton(self.centralwidget)
+        self.pushButton_7.setObjectName(u"pushButton_7")
+        self.pushButton_7.setGeometry(QRect(10, 250, 251, 101))
+        self.pushButton_7.setFont(font)
+        self.pushButton_8 = QCustomQPushButton(self.centralwidget)
+        self.pushButton_8.setObjectName(u"pushButton_8")
+        self.pushButton_8.setGeometry(QRect(270, 250, 251, 101))
+        self.pushButton_8.setFont(font)
+        self.pushButton_9 = QCustomQPushButton(self.centralwidget)
+        self.pushButton_9.setObjectName(u"pushButton_9")
+        self.pushButton_9.setGeometry(QRect(530, 250, 251, 101))
+        self.pushButton_9.setFont(font)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.pushButton.setText("")
+        self.pushButton_2.setText("")
+        self.pushButton_3.setText("")
+        self.pushButton_4.setText("")
+        self.pushButton_5.setText("")
+        self.pushButton_6.setText("")
+        self.pushButton_7.setText("")
+        self.pushButton_8.setText("")
+        self.pushButton_9.setText("")
+    # retranslateUi
+
